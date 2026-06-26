f2e161847ae12d575f92cd57e8dc8e418c36e7c3
dongnguyen <dongtruelove@gmail.com>
2026-06-26 17:13:13 +0700

    fix: resolve env

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 // Money Tracker — Client Environment Configuration
 window.env = {
     // API URL to backend. Will be replaced during deployment on Render/Docker
-    API_URL: "http://localhost:8081/api/v1"
+    API_URL: "API_URL_PLACEHOLDER"
 };
 
